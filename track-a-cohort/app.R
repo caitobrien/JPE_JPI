c14ef875761c27632101940f663b2a10d0219319
caitobrien <120421572+caitobrien@users.noreply.github.com>
2024-05-13 15:58:41 -0700

    add plots and starting shiny

--- track-a-cohort/app.R
+++ track-a-cohort/app.R
@@ -1,53 +1,74 @@
 library(shiny)
-library(shinyWidgets)
-library(plotly)
-library(dplyr)
+library(shinydashboard)
 
-# Define UI for application
-ui <- fluidPage(
-  titlePanel("Winter-run Current and Historic Cumulative Salvage"),
-  sidebarLayout(
-    sidebarPanel(
-      pickerInput("year", "Select Year:", choices = unique(cumulativeLAD$WY), options = list(`actions-box` = TRUE, `selected-text-format` = "count > 3", `count-selected-text` = "{0} years selected")),
-      selectInput("type", "Select Type:", choices = unique(cumulativeLAD$Type2), multiple = TRUE),
-      selectInput("type", "Select BiOp grouping:", choices = c("Pre-2009 BiOp", "2009 & 2019 BiOp"), multiple = TRUE)
-    ),
-    mainPanel(
-      plotOutput("plot")
+ui <- dashboardPage(
+  dashboardHeader(title = "STARS Survival Plot"),
+  dashboardSidebar(disable = TRUE),
+  dashboardBody(
+    fluidRow(
+      box(
+        width = 12,
+        column(width = 4,
+        selectInput(
+        inputId = "select_yeartype",
+        label = "Select year type:", 
+        choices = c("Water Year", "Calendar Year"),
+        multiple = FALSE)
+        ),
+        column(
+          width = 4,
+          selectInput(
+                  inputId = "select_metric", 
+                  label = "Select Probability:", 
+                  choices = c("idRoute", "idsurv", "surv"),
+                  multiple = FALSE)
+        ),
+        column(
+          width = 4,
+          selectInput(
+            inputId = "select_years", 
+            label = "Select years of interest:", 
+            choices = 2017:2024,
+            multiple = TRUE,
+            selected = c(2017:2024)
+        )
+      )
+      ),
+    box(
+      width = 12,
+      plotlyOutput("plot")
+      )
+    )
     )
   )
-)
 
-# Define server logic
-server <- function(input, output) {
-  output$plot <- renderPlot({
-    req(input$year)
-    req(input$type)
+server <- function(input, output, session) {
+  output$plot <- renderPlotly({
+    # Load data
+    files <- list.files(path = here::here('track-a-cohort/shared files/STARS/'))
+    df_stars <- read_csv(here::here(paste0('track-a-cohort/shared files/STARS/',files))) %>% 
+      bind_rows() %>% 
+      select(1, surv = 2, survL80 = 3, survU80 = 4, idsurv = 17, idsurvL80 = 18, idsurvU80 = 19, idRoute = 32, idRouteL80 = 33, idRouteU80 = 34) %>% 
+      arrange(Date) %>% 
+      mutate(WY = year(Date) + (month(Date) >= 10),
+             wDay = if_else(month(Date) >= 10, yday(Date) - 273, yday(Date) + 92),
+             doy = yday(Date),
+             CY = year(Date),
+             wDate = if_else(month(Date) >= 10, Date + years(1), Date))
     
-    data <- cumulativeLAD %>% 
-      filter(WY %in% input$year, Type2 %in% input$type, Status %in% input$Status )
+    # Filter data
+    filtered_data <- df_stars %>% 
+      filter(WY %in% input$select_years,
+             CY %in% input$select_years)
     
-    yearsLAD <- data %>% group_by(WY, Status, TYPE, Type2) %>% summarize(Day = max(wDay), Loss = max(cumloss))
-    max2024LAD <- data %>% filter(WY == 2024 & cumloss == max(cumloss)) %>% pull(cumloss)
-    loss2024LAD <- data %>% filter(WY == 2024) %>% select(10, 11)
+    if(input$select_yeartype == "Water Year") {
+      p<-fct_stars_survival_plot(filtered_data, x_var = "wDay", year = "WY", input$select_metric)
+    } else {
+      p<-fct_stars_survival_plot(filtered_data, x_var = "doy", year = "CY", input$select_metric)
+    }
     
-    
-    WR <- ggplot(data = data) +
-      geom_line(filter(data, WY < 2024), 
-                mapping = aes(x = wDay, y = cumloss, group = factor(WY)), color = 'grey', linewidth = 1)+
-      geom_line(loss2024LAD, 
-                mapping = aes(x = wDay, y = cumloss), color = 'steelblue2', linewidth = 1.5)+
-      geom_text_repel(filter(yearsLAD, Loss > max2024LAD), mapping = aes(x = Day+1, y = Loss, label = WY), 
-                      size = 3, fontface = 'bold', alpha = 0.75, min.segment.length = .1) +
-      facet_grid(Status ~ Type2) +
-      labs(x = 'Date', y = 'Cumulative Loss', title = 'Winter-run Current and Historic Cumulative Salvage') +
-      scale_x_continuous(breaks = c(93,152,213,274), labels = c('Jan', 'Mar', 'May', 'Jul')) +
-      theme(plot.margin = margin(0.5,0.5,0.25,0.25, unit = 'cm'),
-            axis.title.x = element_text(margin=margin(t=10)),
-            axis.title.y = element_text(margin=margin(r=10)))
-    WR
+   return(p)
   })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
